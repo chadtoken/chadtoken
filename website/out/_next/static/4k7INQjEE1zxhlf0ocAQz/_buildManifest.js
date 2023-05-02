37190901454501a5f312fc187bc44ae961e619f5
chadtokenadmin <tokenchad@proton.me>
2023-05-02 00:51:30 -0400

    update out

---
+++ website/out/_next/static/4k7INQjEE1zxhlf0ocAQz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/92-3b9cda6827eb40cf.js","static/chunks/pages/index-dc61d0f5be5f8928.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- website/out/_next/static/w2bsP_nDWTkF2odKjKues/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/92-3b9cda6827eb40cf.js","static/chunks/pages/index-0fd22659fd166edb.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
