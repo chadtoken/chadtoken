f0674956929b659edef0179750f185708bda5adb
chadtokenadmin <tokenchad@proton.me>
2023-05-04 16:08:51 -0400

    update stake page

--- website/pages/stake.tsx
+++ website/pages/stake.tsx
@@ -3,6 +3,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import AppBar from '../components/AppBar';
 import Footer from '../components/Footer';
 
@@ -27,12 +31,21 @@ export default function Album() {
               color="text.primary"
               gutterBottom
             >
-              Mint $YES
+              Staking
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              By staking $CHAD, $PEPE, $WOJAK, or $XEN.
+              Mint $YES by staking $CHAD, $PEPE, $WOJAK, or $XEN
             </Typography>
-            
+            <Stack
+              alignItems="center"
+              justifyContent="center">
+              <Box sx={{ width: { xs: '100%', md: 555 } }}>
+                <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
+                  <Button size="large">Deposit</Button>
+                  <Button size="large">Withdraw</Button>
+                </ButtonGroup>
+              </Box>
+            </Stack>
             <Footer />
           </Container>
         </Box>
